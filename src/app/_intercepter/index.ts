6d2a40c4348a2ec94d8979c9a557df223b760b66
Takahashi Masaki <bje35149@gmail.com>
2022-02-06 10:21:12 +0900

    絶対パスの設定、serviceの依存性の注入

--- src/app/_intercepter/index.ts
+++ src/app/_intercepter/index.ts
@@ -1,6 +1,10 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ErrorInterceptor } from 'src/app/_intercepter/error.interceptor';
+import { JWTInterceptor } from 'src/app/_intercepter/jwt.intetceptor';
+import { backendInterceptor } from 'src/app/_intercepter/backend.intercepter';
 
 export const HttpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, userClass: JWTInterceptor, multi: true},
   { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  { provaide: HTTP_INTERCEPTORS, useClass: backendInterceptor, multi: true }
 ];
