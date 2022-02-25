05fecd9038752cd33bbfbe7ad982a1823b12c8d2
Takahashi Masaki <bje35149@gmail.com>
2022-02-25 10:27:57 +0900

    httpを修正

--- src/app/_intercepter/jwt.intetceptor.ts
+++ src/app/_intercepter/jwt.intetceptor.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AuthenticationService } from '../_service/authentication.service';
