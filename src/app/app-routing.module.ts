3cac11cc6302f11d56e61651dfe158faf9910855
Takahashi Masaki <bje35149@gmail.com>
2022-02-25 13:42:07 +0900

    AuthGuardの修正

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { AuthGuard } from 'src/app/_helper/auth.guard'
 
 const routes: Routes = [
-  { path: '', component: LoginComponent, canActivate: [AuthGuard] },
+  { path: '', component: LoginComponent },
   {
     path: 'mypagetop',
     loadChildren: () =>
       import('./login/mypage-top/mypage-top.module').then((m) => m.mypagetopModule),
-  }
+      canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
