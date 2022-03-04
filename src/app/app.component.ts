dd13bb879c9c7384c443d46fcfb438d7fd60970f
Takahashi Masaki <bje35149@gmail.com>
2022-03-04 10:01:46 +0900

    ログアウト機能の追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { User } from './_models/user';
+import { AuthenticationService } from 'src/app/_service/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
+  
   public currentUser?: User;
 
-  constructor(){}
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
+
+  logout(): void {
+    this.authenticationService.logout();
+    this.router.navigate(['']);
+  }
 }
